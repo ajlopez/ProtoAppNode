38922c0fe64d99b87e2beab8c3a2fc137f469c80
ajlopez <webmaster@ajlopez.com>
2012-05-24 11:23:52 -0300

    dynamic sample

---
+++ samples/dynamic/server.js
@@ -0,0 +1,23 @@
+var protoapp = require('../../');
+var fs = require('fs');
+var express = require('express');
+
+// Load model
+
+var model = JSON.parse(fs.readFileSync('./model.json').toString());
+
+// Create application using model
+
+var application = protoapp.createApplication(model);
+
+express.createServer()
+  .use(express.favicon())
+  .use(express.logger())
+  .use('/content', express.static(__dirname + '/content'))
+  .use(express.router(function(app){
+        // Register application in server routes
+		application.register(app);
+  })).listen(8000);
+  
+console.log('listening to http://localhost:8000');
+
